29371521b0fc078e3ef64d2833ffa78824b7bb4d
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2022-03-18 16:08:38 -0500

    Update Main.java

--- moveArround/Main.java
+++ moveArround/Main.java
@@ -1,49 +1,22 @@
 package moveArround;
-import javax.swing.*;
+import java.awt.*;
+import javaz.swing.*;
 
 public class Main extends JFrame{
+  Dimention size = new Dimention(1000,750);
+  
   public Main(){
-    setSize(1000,750);
-    setResizeable(false);
-    setDefaultCloseOpperation(JFrame.EXIT_ON_CLOSE);
+    setSize(size.width,size.height);
+    setResizable(false);
     setTitle("Move Arround");
-    ImageIcon image=new ImageIcon("character.png");
-    Player player=new Player(image);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JLabel playerContainer = new JLabel(new ImageIcon("player.png"));
+    Player player = new Player(playerContainer);
     pack();
   }
-  public void keyPressed(KeyEvent evt){
-    int key=evt.getKeyCode();
-    if (key==KeyEvent.VK_LEFT){
-      if (player.x>0){
-        player.setX(player.x-player.speed);
-      }else{
-        player.setX(0);
-      }
-    }
-    if (key==KeyEvent.VK_RIGHT){
-      if (player.x<(1000-player.size.width)){ //TODO: Create:  Dimention Player.size
-        player.setX(player.x+player.speed);  //TODO: Player.setX
-      }else{
-        player.setX(1000-player.size.width);
-      }
-    }
-    if (key==KeyEvent.VK_UP){
-      if (player.y>0){
-        player.setY(player.y-player.speed);
-      }else{
-        player.setY(0);
-      }
-    }
-    if (key==KeyEvent.VK_DOWN){
-      if (player.y<750){
-        player.setY(player.y+player.speed);
-      }else{
-        player.setY(750-player.size.height);
-      }
-    }
-  }
+  
   public static void main(String[] args){
-    javax.swing.SwingUtilities.invokeLater(new Runnable(){
+    SwingUtilities.invokeLater(new Runnable(){
       @Override
       public void run(){
         new Main().setVisible(true);
@@ -51,5 +24,4 @@ public class Main extends JFrame{
     });
   }
 }
-
-//Thank kyuu >w<
+//Thank Kyuu >w<
