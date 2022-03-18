6a9b8e0bf46cfd52f4f1702b694fc6bf470b8f04
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2022-03-18 17:14:22 -0500

    Update Main.java

--- moveArround/Main.java
+++ moveArround/Main.java
@@ -2,7 +2,7 @@ package moveArround;
 import java.awt.*;
 import javaz.swing.*;
 
-public class Main extends JFrame{
+public class Main extends JFrame implements KeyListener, FocusListener{
   Dimention size = new Dimention(1000,750);
   
   public Main(){
@@ -12,16 +12,41 @@ public class Main extends JFrame{
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     JLabel playerContainer = new JLabel(new ImageIcon("player.png"));
     Player player = new Player(playerContainer);
+    JPanel focusBox = new JPanel();
+    focusBox.setBounds(100,100,size.width-100,size.height-100);
+    focusBox.setBackground(Color.Grey);
+    JLabel focusLabel = new JLabel("Click to gain focus.");
+    focusLabel.setBounds(20,20,size.width-120,size.height-120);
+    focusLabel.setVerticalTextPosition(JLabel.CENTER);
+    focusLabel.setHorazontalTextPosition(JLabel.CENTER);
+    focusBox.add(focusLabel);
+    addFocusListener(this);
+    addKeyListener(this);
+    add(playerContainer);
+    add(focusBox);
+    focusBox.setVisible(false);
     pack();
   }
+  public void FocusLost(FocusEvent e){
+    focusBox.setVisible(true);
+  } 
+  public void FocusGained(FocusEvent e){
+    focusBox.setVisible(false);
+  }  
+  public void keyPressed(KeyEvent e){
+    player.keyboardEvent(e.getKeyCode());
+  }
+
   
   public static void main(String[] args){
+    System.out.println("Loading...");
     SwingUtilities.invokeLater(new Runnable(){
       @Override
       public void run(){
         new Main().setVisible(true);
       }
     });
+    System.out.println("Done!");
   }
 }
 //Thank Kyuu >w<
