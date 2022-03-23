a603c30f93157344d5d9d3f17fec8c73050f32c0
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2022-03-22 21:18:20 -0500

    Update ButtonListener.java

--- compilerGUI/ButtonListener.java
+++ compilerGUI/ButtonListener.java
@@ -1,4 +1,5 @@
 package compilerGUI;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -6,13 +7,18 @@ import java.awt.event.*;
 public class ButtonListener extends ActionListener{
     private JTextField src,dest;
     private Compile compile;
-    public ButtonListener(JTextField sourceBox, JTextField buildBox, Compile compile){
+    private ErrorDialog dialog;
+    public ButtonListener(JTextField sourceBox, JTextField buildBox, Compile compile, ErrorDialog errordialog){
         this.src = sourceBox;
         this.dest = buildBox;
         this.compile = compile;
+        this.dialog = errordialog;
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        compile.build(src.getText(),dest.getText());
+        ArrayList<String> out = compile.build(src.getText(),dest.getText());
+        if (out.size()>0){
+            dialog.setErrorText(out);
+        }
     }
 }
